4634c85b17b9b4bfc0d21bdb3d61293e819956f7
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-02-14 17:00:40 +0800

    Add files via upload

---
+++ gjb013.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	string a,b,c;
+	getline(cin,a);
+	getline(cin,b);
+	getline(cin,c);
+	cout<<c<<endl;
+	cout<<a<<endl;
+	cout<<b;
+	return 0;
+}
