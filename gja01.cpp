ed435a623cefc375e4907db78b565e6fbdaed23d
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-04-15 17:23:03 +0800

    Add files via upload

---
+++ gja01.cpp
@@ -0,0 +1,7 @@
+#include  <iostream>/*這是標頭檔，你用不同標頭檔有不同函式庫，然後iostream功能包括最基礎的輸出輸入流*/  
+using namespace std;/*這一行等於說使用 namespace 來定義名稱空間，聽不懂沒關係，反正記得要寫，很重要*/  
+/*然後是主程式區*/  
+int main(){  
+    cout << "Hello!"/*cout是輸出的意思，雙字符刮起來的是你輸出的字串*/;  
+    return 0;/*回傳值，不重要，記得寫就對了*/  
+}  
