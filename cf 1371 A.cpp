3bf300cc98ef4a4cf4a97fb4c7142fd9ae07e3ea
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-07-11 19:09:00 +0800

    Add files via upload

---
+++ cf 1371 A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef long long ll;
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<(n+1)/2<<endl;
+	} 
+	return 0;
+}
