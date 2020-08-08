afb0eefd83ca3084c48fe39c6ec43f206322bbc2
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-08-08 17:19:47 +0800

    Add files via upload

---
+++ cf 1393 A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+vector<int> v;
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<n/2+1<<endl;
+		
+	}
+	return 0;
+}
