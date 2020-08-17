1a8f2400e3234b1e688720c820cdc5a8610d05d4
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-08-17 21:43:20 +0800

    Add files via upload

---
+++ AP325 P-4-2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+int arr[100005];
+int b[100005];
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++) cin>>arr[i];
+	for(int i=0;i<n;i++) cin>>b[i];
+	sort(arr,arr+n);
+	sort(b,b+n);
+	int pos=0;
+	int ans=0;
+	for(int i=0;i<n;i++){
+		while(pos<n){
+			if(arr[pos]>b[i]){
+				pos++;
+				ans++;
+				break;
+			}
+			pos++;
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
+}
