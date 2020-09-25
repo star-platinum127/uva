60ec88af752fb9cbb40b42098a766bf421fff4da
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-09-25 19:45:38 +0800

    Create cf 1420 B.cpp

---
+++ cf 1420 B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define MOD 1000000007
+const int MAX = 100005;
+const int INF= 0x3F3F3F3F;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+int arr[100005];
+map<int, int> mp;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		ll ans = 0;
+		cin >> n;
+		mp.clear();
+		for (int i = 0;i < n;i++) {
+			cin >> arr[i];
+			int tmp = __lg(arr[i]);
+			ans += mp[tmp];
+			mp[tmp]++;
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
