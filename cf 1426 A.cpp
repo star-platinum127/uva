55d039e82cd1e917a269d9bcd5a6724de6415c8e
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-10-01 20:12:27 +0800

    Create cf 1426 A.cpp

---
+++ cf 1426 A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define MOD 1000000007
+const int MAXN = 100005;
+const int INF= 0x3F3F3F3F;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+/*------------------------------------------*/
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m;
+		cin>>n>>m;
+		n-=2;
+		int cnt=1;
+		if(n<=0){
+			cout<<cnt<<endl;
+			continue;
+		}
+		cnt+=(n/m);
+		if(n%m) cnt++;
+		cout<<cnt<<endl;
+	}
+	return 0;
+} 
