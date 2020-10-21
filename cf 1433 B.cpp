e239a91adb70d93452e38a04cfbf5a3cf91940f8
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-10-21 22:11:11 +0800

    Create cf 1433 B.cpp

---
+++ cf 1433 B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+const ll MOD = 1000000007;
+const int MAXN = 100005;
+const int INF= 0x3F3F3F3F;
+const long long LINF = 4611686018427387903;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+/*-----------------------------------------------*/
+
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> arr;
+		for (int i = 0;i < n;i++) {
+			int tmp;
+			cin >> tmp;
+			if (tmp) arr.pb(i);
+		}
+		int len = arr.size();
+		ll ans =arr[len-1]-arr[0]-1-(len-2);
+		cout << ans << "\n";
+	}
+	return 0;
+}
