e903c2368b5d14c8b8faad08d5428ef4c366d189
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-10-24 23:47:40 +0800

    Create cf 1428 A.cpp

---
+++ cf 1428 A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define lowbit(x) x&-x
+const int MOD = 1000000009;
+const int MAXN = 100005;
+const int INF= 0x3F3F3F3F;
+const long long LINF = 4611686018427387903;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+/*-----------------------------------------------*/
+
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		int a = abs(x2 - x1),b=abs(y2-y1);
+		if (a && b) cout << a + b + 2 << "\n";
+		else cout << a + b << "\n";
+	}
+	return 0;
+}
