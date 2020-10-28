33de359832c02dabbc9504e4aca764f6e67cfa9a
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-10-28 21:44:02 +0800

    Create cf 1436 B.cpp

---
+++ cf 1436 B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define miku mywaifu
+#define misaka mywaifu
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define lowbit(x) x&-x
+const ll MOD = 1000000007;
+const int MAXN = 100005;
+const int INF= 0x3F3F3F3F;
+const long long LINF = 4611686018427387903;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+/*-----------------------------------------------*/
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			int pos=(i+1)%n;
+			for(int j=0;j<n;j++){
+				if(j==i || j==pos) cout<<1<<" ";
+				else cout<<0<<" ";
+			}
+			cout<<"\n";
+		}
+		
+	}
+}
