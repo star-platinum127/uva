d63ed8281622d7b910ef6dad4fd4f4ffe7bab06e
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2020-12-13 21:47:14 +0800

    Create cf 1452 A.cpp

---
+++ cf 1452 A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t ll;
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define mikumywaifu ios_base::sync_with_stdio(0)
+#define misakamywaifu cin.tie(0)
+#define pb push_back
+#define S second
+#define F first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define lowbit(x) x&-x
+const int MOD = 1000000007;
+const int INF= 0x3F3F3F3F;
+const long long LINF = 4611686018427387903;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+template<typename _Size>
+inline _Size
+__lg(_Size __n)
+{
+	_Size __k;
+	for (__k = 0; __n != 0; __n >>= 1)
+		++__k;
+	return __k - 1;
+}
+/*-----------------------------------------------*/
+
+signed main() {
+	mikumywaifu;
+	misakamywaifu;
+	int t;
+	cin >> t;;
+	while (t--) {
+		int x, y;
+		cin >> x >> y;
+		if (x == y) cout << x + y << "\n";
+		else cout << max(x, y) * 2 - 1 << "\n";
+	}
+	return 0;
+}
