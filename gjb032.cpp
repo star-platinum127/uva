61656c3fc5a21201fce79f699f80a52de47022b2
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2021-05-23 16:25:07 +0800

    Update gjb032.cpp

--- gjb032.cpp
+++ gjb032.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+
+int main() {
 	int c,co=0,most=0;
 	cin>>c;
 	int a[c][2];
